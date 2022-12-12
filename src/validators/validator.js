45e77b894718e5b91f137da65ef88bc53dfca024
Sagar <sagarmaan2015@gmail.com>
2022-12-12 10:13:57 +0530

    validators

---
+++ src/validators/validator.js
@@ -0,0 +1,27 @@
+const isValidName = function (name) {
+    const nameRegex = /^[a-z ,A-Z]+$/;
+    return nameRegex.test(name)
+}
+
+const isValidNumber= function(invoice){
+    const nameRegex = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/
+    return nameRegex.test(invoice)
+}
+
+const isValidEmail=function(email){
+    const pattern =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return  pattern.test(email)
+}
+
+const isValidNames = function (name) {
+    const nameRegex = /^[a-zA-Z ]{1,30}$/;;
+    return nameRegex.test(name)
+}
+
+
+module.exports={isValidName,isValidEmail, isValidNumber , isValidNames}
+
+
+
+
+
