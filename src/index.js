74886b15b468e7ed12f3c102d02a2846ba5fef35
Sagar <sagarmaan2015@gmail.com>
2022-12-13 10:15:19 +0530

    routes , index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+const route = require("./routes/routes.js")
+
+app.use(express.json())
+
+
+mongoose.set('strictQuery', true)
+mongoose.connect("mongodb+srv://anjalis1509:anjalis1509@cluster0.rhdux0p.mongodb.net/group2Database", {
+    useNewUrlParser: true
+}).then(()=>console.log("MongoDb is connected"))
+.catch(err=>console.log(err))
+
+app.use("/", route)
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server is running on port ${process.env.port||3000}`)
+})
---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router  = express.Router()
+const {createCollege, getCollege} = require("../controller/collegeController")
+const {createIntern} = require("../controller/internController")
+
+
+
+
+router.post( "/functionup/colleges" , createCollege )
+
+router.get("/functionup/collegeDetails", getCollege)
+
+router.post( "/functionup/interns" , createIntern )
+
+
+module.exports= router
